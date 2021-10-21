fcc3702aa8197c6e08ca914bdae87d6f3a008148
David Swartwood <david.swartwood@att.net>
2021-10-21 10:33:32 -0700

    new file:   fwn_app/forms.py 	modified:   fwn_app/models.py 	modified:   fwn_app/templates/fwn_app/Idea.html 	modified:   fwn_app/templates/fwn_app/base.html 	modified:   fwn_app/templates/fwn_app/cal.html 	modified:   fwn_app/templates/fwn_app/house.html 	modified:   fwn_app/urls.py 	new file:   fwn_app/utils.py 	modified:   fwn_app/views.py 	modified:   users/templates/users/login.html 	modified:   users/templates/users/register.html

--- fwn/fwn_app/urls.py
+++ fwn/fwn_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 # Need calendar path with date and UUID
 app_name = 'fwn_app' # for namespacing
 urlpatterns = [
     path('', views.index, name='index'),
+    path('calendar/$', views.CalendarView.as_view(), name='calendar'),
     path('<int:year>/<str:month>',views.calendar, name='calendar'),
-    path('<house>',views.calendar, name='calendar'),
+    path('<house>',views.household, name='house'),
 
 ]
--- fwn/fwn_app/views.py
+++ fwn/fwn_app/views.py
@@ -1,13 +1,67 @@
 from django.shortcuts import render, get_object_or_404,redirect
-from datetime import datetime
-from .models import Idea, Event, House
-from django.template import loader
-from django.urls import reverse
-from django.http import HttpResponse
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from calendar import HTMLCalendar, month_name
-from datetime import datetime
-# Create your views here.
+from datetime import datetime , timedelta, date
+from django.utils.safestring import mark_safe
+from django.views import generic
+
+from .models import *
+from .utils import Calendar
+from .forms import EventForm
+
+
+#Creating calendar views using code from https://github.com/huiwenhw/django-calendar/tree/master/cal
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'fwn_app/cal.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) #call the method on the parent with super
+        d = get_date(self.request.GET.get('month', None))
+        cal = Calendar(d.year, d.month)
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+        return context
+
+def get_date(req_month):
+    if req_month:
+        year, month = (int(x) for x in req_month.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('cal:calendar'))
+    return render(request, 'cal/event.html', {'form': form})
+
+
+
+
 def index(request):
     # return HttpResponse('ok') #test
     if request.method == 'POST':
@@ -26,7 +80,7 @@ def index(request):
 def household(request, profile):
     return render(request, "fwn_app/house.html",
     {
-        'house':House.house,
+        'house':House,
         
     })
    
@@ -49,5 +103,8 @@ def calendar(request, year, month):
     })
 
 def idealist(request):
-    
-    return HttpResponse('ok')
+    return render(request, "fwn_app/idea.html",
+    {
+        'idea':Idea,
+        
+    })
